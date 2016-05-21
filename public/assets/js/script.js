83b516534e1558bc73955a89196552a921e18dd0
James Candan <jcandan82@gmail.com>
2016-05-21 17:06:59 -0400

    on compare click, get products marked for comparison.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,4 +8,11 @@ jQuery(function($){
       .fail(function() {
           console.log('error parsing json.');
       });
+
+    $('#compare-btn').click(function(ev){
+        var checked_values = $('input[name=compare]:checked').map(function() {
+            return this.value;
+        }).get();
+        console.log(checked_values);
+   });
 });
