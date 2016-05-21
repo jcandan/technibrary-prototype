88a4cb2d7c64f0006a20c2a13cec4d1c9e502a37
James Candan <jcandan82@gmail.com>
2016-05-21 14:35:20 -0400

    use handlebars to output products to html from json data.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,9 @@
 jQuery(function($){
-    var jqxhr = $.getJSON( "data/data.json", function() {
-      console.log("successfully parsed json.")
+    var jqxhr = $.getJSON( "data/data.json", function(data) {
+        $.get("templates/product.tpl", function(tpl) {
+            var template = Handlebars.compile(tpl);
+            $('#products').html(template(data));
+        })
     })
       .fail(function() {
           console.log('error parsing json.');
