87751a56ef1fec373e4bdee0daf8d9e62fa4cb16
James Candan <jcandan82@gmail.com>
2016-07-06 23:53:21 +0000

    remove javascript call for Handlebars.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,29 +1,3 @@
-jQuery(function($){
-    var jqxhr = $.getJSON( "assets/data/data.json", function(data) {
-        $.get("assets/templates/product.hbs", function(hbs) {
-            var template = Handlebars.compile(hbs);
-            $('#products').html(template(data));
-        })
-    })
-      .fail(function() {
-          console.log('error parsing json.');
-      });
+(function($){
 
-    $('#compare-btn').click(function(ev){
-        var checked_values = $('input[name=compare]:checked').map(function() {
-            return this.value;
-        }).get();
-        jqxhr.complete(function(){
-            var all_products = jqxhr.responseJSON.products;
-            var products = [];
-            $.each(checked_values, function(key, val) {
-                products.push(all_products[val]);
-            });
-
-            $.get("assets/templates/comparison_table.hbs", function(hbs) {
-              var template = Handlebars.compile(hbs);
-              $('.modal-body').html(template(products));
-            });
-        });
-   });
-});
+})(jQuery);
